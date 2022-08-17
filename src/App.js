b4305c94894fe77402f85ddbe165327f370cc969
fel <felite333@gmail.com>
2022-08-17 16:24:15 +0200

    3

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./public/logo1.png" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <img src={require('.//logo1.png')} className="App-logo" alt="logo" />       
        
       </header>
+      <img src={require('.//logo1.png')}  alt="ggg" />
     </div>
   );
 }
