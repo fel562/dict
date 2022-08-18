8b846ae1e39cb9787c9b7dddd290cc4d4b14ae5f
fel <felite333@gmail.com>
2022-08-18 14:15:51 +0200

    3

--- src/Results.js
+++ src/Results.js
@@ -1,24 +1,22 @@
 import React from "react";
 import "./App.css";
 
-
 export default function Results(props) {
   if (props.results) {
     return (
-      <div className="result  pt-5 m-0 bg-white">
+      <div className="result">
         <h2>{props.results.word}</h2>
+        {props.results.phonetic}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-                 <h3>{props.meaning.partOfSpeech}</h3>
+              <h3>{props.meaning.partOfSpeech}</h3>
             </div>
           );
         })}
-        
       </div>
     );
   } else {
     return null;
   }
 }
-
