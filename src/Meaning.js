53a0a01b4f80bee7bc6877e3d642aca91a2764d7
fel <felite333@gmail.com>
2022-08-24 17:58:28 +0200

    f

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="meanings">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <h4>{props.meaning.definitions[0].definition}</h4>
-      <h4>{props.meaning.definitions[0].example }</h4>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong>
+              {definition.definition}
+              <br />
+              <strong>Example:</strong>
+              <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+              <br />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
